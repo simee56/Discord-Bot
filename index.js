3597d7321a840e98f8f2810e454445bffce6e41c
simee56 <pandeysimee7@gmail.com>
2025-02-13 19:55:45 +0530

    import discord, create a client and reply thr message using token

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents:
+        [GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent]
+});
+
+client.on("messageCreate", (message) => {
+    if(message.author.bot)
+        return;
+
+    message.reply({
+        content : "Hello form Bot",
+    });
+
+    console.log(message) // this will display the message
+    // console.log(message.content); this will display the message as well as other informations such as user's info.
+
+ 
+})
+
+client.login (
+    "MTMzOTU2NjkzNjMzNzU1MTM2MQ.GAniIh.eUmqPPc3nxMX8AtrKpkC25iK4g-I3vxYnyL5Jo"
+)
